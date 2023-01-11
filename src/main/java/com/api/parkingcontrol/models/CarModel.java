23e4b5c60d72c3c2054d672fa41f900e79d280e9
johnjoker13 <joaoavelino2008@hotmail.com>
2023-01-11 11:16:45 -0300

    feat(src/main/parkingcontrol/CarModel.java): creates Car Entity

---
+++ src/main/java/com/api/parkingcontrol/models/CarModel.java
@@ -0,0 +1,87 @@
+package com.api.parkingcontrol.models;
+
+import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_CAR")
+public class CarModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID id;
+    
+    @Column(nullable = false, unique = true, length = 7)
+    private String licensePlateCar;
+    
+    @Column(nullable = false, length = 70)
+    private String brandCar;
+    
+    @Column(nullable = false, length = 10)
+    private String modelCar;
+    
+    @Column(nullable = false, length = 10)
+    private String colorCar;
+    
+    @OneToOne(mappedBy = "car", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private ParkingSpotModel parkingSpot;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getLicensePlateCar() {
+        return licensePlateCar;
+    }
+
+    public void setLicensePlateCar(String licensePlateCar) {
+        this.licensePlateCar = licensePlateCar;
+    }
+
+    public String getBrandCar() {
+        return brandCar;
+    }
+
+    public void setBrandCar(String brandCar) {
+        this.brandCar = brandCar;
+    }
+
+    public String getModelCar() {
+        return modelCar;
+    }
+
+    public void setModelCar(String modelCar) {
+        this.modelCar = modelCar;
+    }
+
+    public String getColorCar() {
+        return colorCar;
+    }
+
+    public void setColorCar(String colorCar) {
+        this.colorCar = colorCar;
+    }
+
+    public ParkingSpotModel getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(ParkingSpotModel parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+    
+}
